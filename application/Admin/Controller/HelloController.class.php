3fde313afd9fd6c68a784087a7c84aba76b831a4
aierlan <752677266@qq.com>
2017-05-11 21:31:54 +0800

    第二次提交

---
+++ application/Admin/Controller/HelloController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\AdminbaseController;
+class HelloController extends AdminbaseController{
+
+	
+	public function helloworld()
+	{
+		$this->display();
+	}
+}
+?>
